576b22782600a4fb9e65e3024c5d933a0f24c1b9
Andrew Y Xie <andyxie@D-173-250-197-238.dhcp4.washington.edu>
2017-05-18 16:48:58 -0700

    not working top bar

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ReverseTabs from './components/navigation/ReverseTabs';
+import ReverseTopTabs from './components/navigation/ReverseTopTabs';
 export default class App extends React.Component {
   render() {
     return (
-      <ReverseTabs />
+      <ReverseTopTabs />
     );
   }
 }
