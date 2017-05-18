5c9923ab0f456077e87e72090092f434be7c84b7
Andrew Y Xie <andyxie@D-173-250-197-238.dhcp4.washington.edu>
2017-05-18 14:17:58 -0700

    add navigation examples

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import MyNav from './components/App';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-      </View>
+      <MyNav />
     );
   }
 }
